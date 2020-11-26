0b72beac1737d9e4ac603e3a046ac52d26fc4f62
Asher <51806917+asher-lab@users.noreply.github.com>
2020-11-26 17:51:57 +0800

    Update RPC_Balance_Checker.py

--- PythonCodes/With_API_Call/RPC_Balance_Checker.py
+++ PythonCodes/With_API_Call/RPC_Balance_Checker.py
@@ -8,10 +8,14 @@ result = requests.get(url)
 src = result.text
 data = json.loads(src)
 
-print("HAC: ")
-print(data['list'][0]['hacash'])
-print("BTC: ")
-print (data['list'][0]['satoshi'])
-print("HACD: ")
-print(data['list'][0]['diamond'])
+if (data['ret'] == 0):
+    print("HAC: ")
+    print(data['list'][0]['hacash'])
+    print("BTC: ")
+    print (data['list'][0]['satoshi'])
+    print("HACD: ")
+    print(data['list'][0]['diamond'])
+    
+if (data['ret'] == 1):
+    print("Query FAILED: " + (data['errmsg']))
 
